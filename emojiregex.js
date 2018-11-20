1a129d6afd5c28823576bca6cbd938334c4e478f
Steven Tang <TangSteven@users.noreply.github.com>
2018-11-19 18:41:34 -0700

    Fix minified version.

--- emojiregex.js
+++ emojiregex.js
@@ -1,27 +1,25 @@
 const emojiRegex = fetch('https://httpsify.xeodou.me/url?redirect=http://www.unicode.org/Public/emoji/latest/emoji-data.txt') //bypass Mixed Content
   .then(response => response.text())
   .then(data => {
-    let pattern = '';
+    let pattern = [];
     const unicode = data.replace(/^#.*?\n/gm, '').replace(/;.*?\n/gm, '||').replace(/\n/gm, '').split('||');
     unicode.pop();
-    unicode.forEach((code, index) => {
+    unicode.forEach(code => {
       code = code.trim();
       switch (code.length) {
         case 4:
         case 5:
-          pattern += `\\u{${code}}`;
+          pattern.push(`\\u{${code}}`);
           break;
         case 10:
         case 12:
           var codes = code.split('..');
-          pattern += `[\\u{${codes[0]}}-\\u{${codes[1]}}]`;
+          pattern.push(`[\\u{${codes[0]}}-\\u{${codes[1]}}]`);
           break;
       }
-      if (index < unicode.length - 1) {
-        pattern += '|';
-      }
     });
+    pattern = pattern.join('|');
     //Disincludes NUMBER SIGN(#), ASTERISK(*), and DIGIT ZERO..DIGIT NINE(0-9)
-    pattern = pattern.replace(/\\\u\{0023\}\|\\\u\{002A\}\|\[\\\u\{0030\}\-\\\u\{0039\}\]\|/g, '');
+    pattern = pattern.replace(/\\\u\{0023\}\|\\\u\{002A\}\|\[\\\u\{0030\}-\\\u\{0039\}\]\|/g, '');
     return new RegExp(pattern, 'u');
   }).catch(e => console.error(e));
